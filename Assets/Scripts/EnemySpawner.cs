0facc176bc88faf2a4d8716f0d2b31a03e2698de
AbtahiChowdhury <abtahichowdhury@gmail.com>
2020-05-02 16:57:39 -0400

    redid enemy spawning and movement speed

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,41 +4,107 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject enemyPrefab;
+    public GameObject EnemyPrefab;
 
-    public float roundStartTime;
+    public Vector2 StageSize;
+
+    public float RoundStartTime;
 
     int spawnTableCounter = 0;
-    float[,] spawnTable = new float[,]
+    float[] spawnTable = new float[]
     {
-        //spawn_time, x_pos, y_pos, z_pos
-        { 2f, 0f, 0f, 4.12f },
-        { 4f, 0f, 0f, 4.12f },
-        { 6f, 0f, 0f, 4.12f },
-        { 8f, 0f, 0f, 4.12f },
-        { 10f, 0f, 0f, 4.12f },
-        { 12f, 0f, 0f, 4.12f },
+        //spawn_times
+        2f, 4f, 6f, 8f, 10f, 12f, 14f, 16f, 18f, 20f,
     };
 
     // Start is called before the first frame update
     void Start()
     {
         //tmp round start time, set to current time at start of round
-        roundStartTime = Time.time;
+        RoundStartTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (spawnTableCounter < spawnTable.GetLength(0) && (Time.time - roundStartTime) > spawnTable[spawnTableCounter, 0])
+        if (spawnTableCounter < spawnTable.Length && (Time.time - RoundStartTime) > spawnTable[spawnTableCounter])
         {
-            Vector3 spawnPoint = new Vector3(
-                spawnTable[spawnTableCounter, 1],
-                spawnTable[spawnTableCounter, 2],
-                spawnTable[spawnTableCounter, 3]
-            );
-            Instantiate(enemyPrefab).transform.position = spawnPoint;
+            Instantiate(EnemyPrefab).transform.position = GetSpawnPoint();
             spawnTableCounter++;
         }
+    }
+
+    private Vector3 GetSpawnPoint()
+    {
+        Vector3 spawnPoint = new Vector3();
+        Vector3 playerPos = Player.instance.pos;
+
+        int quadrent = Random.Range(1, 3);
+        float x = Random.Range(0f, StageSize.x / 2f);
+        float y = Random.Range(0f, StageSize.y / 2f);
+        float z = 4.12f;
+
+        if (playerPos.x > 0 && playerPos.y > 0) //quadrent 1
+        {
+            if (quadrent == 1)
+            {
+                spawnPoint = new Vector3(-x, y, z);
+            }
+            else if (quadrent == 2)
+            {
+                spawnPoint = new Vector3(-x, -y, z);
+            }
+            else
+            {
+                spawnPoint = new Vector3(x, -y, z);
+            }
+        }
+        else if (playerPos.x < 0 && playerPos.y > 0) //quadrent 2
+        {
+            if (quadrent == 1)
+            {
+                spawnPoint = new Vector3(-x, -y, z);
+            }
+            else if (quadrent == 2)
+            {
+                spawnPoint = new Vector3(x, -y, z);
+            }
+            else
+            {
+                spawnPoint = new Vector3(x, y, z);
+            }
+        }
+        else if (playerPos.x < 0 && playerPos.y < 0) //quadrent 3
+        {
+            if (quadrent == 1)
+            {
+                spawnPoint = new Vector3(x, -y, z);
+            }
+            else if (quadrent == 2)
+            {
+                spawnPoint = new Vector3(x, y, z);
+            }
+            else
+            {
+                spawnPoint = new Vector3(-x, y, z);
+            }
+        }
+        else //quadrent 4
+        {
+            if (quadrent == 1)
+            {
+                spawnPoint = new Vector3(x, y, z);
+            }
+            else if (quadrent == 2)
+            {
+                spawnPoint = new Vector3(-x, y, z);
+            }
+            else
+            {
+                spawnPoint = new Vector3(-x, -y, z);
+            }
+        }
+
+        return spawnPoint;
     }
 }
